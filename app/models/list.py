ce004d8943e345b5d7beceea990209053c9eabea
brandon <brandon.bim13@gmail.com>
2023-01-23 22:12:13 -0500

    added models boards, lists, and cards

---
+++ app/models/list.py
@@ -0,0 +1,45 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from sqlalchemy.sql import func
+
+class List(db.Model):
+    __tablename__ = 'lists'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    board_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("boards.id")), nullable=False)
+    watched = db.Column(db.Boolean, default=False) #should this be just in frontend?
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    # - Many to One: List has one Board through board_id
+    board = db.relationship('Board', back_populates='lists')
+
+    # - One to Many: List will have many cards through list_id
+    cards = db.relationship('Card', back_populates='list', cascade='all, delete-orphan')
+
+
+    def to_dict(self):
+        """
+        Returns a dict representing List
+        {
+            id,
+            name,
+            board_id,
+            watched,
+            created_at,
+            updated_at,
+            cards
+        }
+        """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'board_id': self.board_id,
+            'watched': self.watched,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+            'cards': [card.to_dict() for card in self.cards]
+        }
