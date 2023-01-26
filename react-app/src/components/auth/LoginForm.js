e6fe9c03d28157a877f7fdaccc1a6b2754c47c3a
brandon <brandon.bim13@gmail.com>
2023-01-26 14:16:28 -0500

    added css for signup and login and functionality for signup

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,73 +2,77 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import './LoginForm.css'
 
 const LoginForm = () => {
-  const [errors, setErrors] = useState([]);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const user = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
+    const [errors, setErrors] = useState([]);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const user = useSelector(state => state.session.user);
+    const dispatch = useDispatch();
 
-  const onLogin = async (e) => {
-    e.preventDefault();
-    const data = await dispatch(login(email, password));
-    if (data) {
-      setErrors(data);
+    const onLogin = async (e) => {
+        e.preventDefault();
+        const data = await dispatch(login(email, password));
+        if (data) {
+            setErrors(data);
+        }
+    };
+
+    const demoUser = async (e) => {
+        e.preventDefault();
+        const data = await dispatch(login('demo@aa.io', 'password'));
+        if (data) {
+            setErrors(data);
+        }
+    };
+
+    const updateEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const updatePassword = (e) => {
+        setPassword(e.target.value);
+    };
+
+    if (user) {
+        return <Redirect to={`/${user.username.toLowerCase()}/boards`} />;
     }
-  };
 
-  const demoUser = async (e) => {
-    e.preventDefault();
-    const data = await dispatch(login('demo@aa.io', 'password'));
-    if (data) {
-      setErrors(data);
-    }
-  };
+    return (
+        <div className='jcc'>
+        <form onSubmit={onLogin} className='loginForm fdc aic g1'>
+            <h4 className='loginHeader aic jcc' style={{"marginTop":"5px"}}>Log in to Mello</h4>
+            <div>
+            {errors.map((error, ind) => (
+                <div key={ind}>{error}</div>
+            ))}
+            </div>
 
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
+            {/* <label htmlFor='email'>Email</label> */}
+            <input
+                name='email'
+                type='text'
+                placeholder='Enter email'
+                value={email}
+                onChange={updateEmail}
+            />
 
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
+            {/* <label htmlFor='password'>Password</label> */}
+            <input
+                name='password'
+                type='password'
+                placeholder='Enter Password'
+                value={password}
+                onChange={updatePassword}
+            />
 
-  if (user) {
-    return <Redirect to={`/${user.username.toLowerCase()}/boards`} />;
-  }
-
-  return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-        <button onClick={demoUser}>Demo User</button>
-    </form>
-  );
+            <button type='submit' className='fwb' id='loginButtons'>Log in</button>
+            <div style={{"fontSize":"12px", "color":"lightSlateGray"}}>OR</div>
+            <button className='fwb' id='demoButton' onClick={demoUser}>Demo User</button>
+        </form>
+        </div>
+    );
 };
 
 export default LoginForm;
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,94 +1,134 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import './SignUpForm.css'
 
 const SignUpForm = () => {
-  const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
+    const [errors, setErrors] = useState([]);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
+    const user = useSelector(state => state.session.user);
+    const dispatch = useDispatch();
 
-  const onSignUp = async (e) => {
-    e.preventDefault();
-    if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
-      if (data) {
-        setErrors(data)
-      }
+    const onSignUp = async (e) => {
+        e.preventDefault();
+        if (password === repeatPassword) {
+        const data = await dispatch(signUp(firstName, lastName, username, email, password));
+        if (data) {
+            setErrors(data)
+        }
+        }
+    };
+
+    useEffect(() => {
+        const e = []
+        if(password !== repeatPassword) e.push("Passwords must match")
+        setErrors(e)
+    }, [password, repeatPassword])
+
+    const updateFirstName = (e) => {
+        setFirstName(e.target.value);
+    };
+
+    const updateLastName = (e) => {
+        setLastName(e.target.value);
+    };
+
+    const updateUsername = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const updateEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const updatePassword = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const updateRepeatPassword = (e) => {
+        setRepeatPassword(e.target.value);
+    };
+
+    if (user) {
+        return <Redirect to={`/${user.username.toLowerCase()}/boards`} />;
     }
-  };
 
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
-  };
+    return (
+        <div className='jcc'>
+            <form onSubmit={onSignUp} className='signUpForm fdc aic g1'>
+            <h4 className='signUpHeader aic jcc' style={{"marginTop":"5px"}}>Sign up for your account</h4>
+            <div>
+                {errors.map((error, ind) => (
+                <div key={ind}>{error}</div>
+                ))}
+            </div>
 
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
+                <input
+                type='text'
+                name='first_name'
+                placeholder='Enter first name'
+                onChange={updateFirstName}
+                value={firstName}
+                required={true}
+                ></input>
 
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
+                <input
+                type='text'
+                name='last_name'
+                placeholder='Enter last name'
+                onChange={updateLastName}
+                value={lastName}
+                required={true}
+                ></input>
 
-  const updateRepeatPassword = (e) => {
-    setRepeatPassword(e.target.value);
-  };
+                <input
+                type='text'
+                name='username'
+                placeholder='Enter username'
+                onChange={updateUsername}
+                value={username}
+                required={true}
+                ></input>
 
-  if (user) {
-    return <Redirect to={`/${user.username.toLowerCase()}/boards`} />;
-  }
+                <input
+                type='text'
+                name='email'
+                placeholder='Enter email'
+                onChange={updateEmail}
+                value={email}
+                required={true}
+                ></input>
 
-  return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
-  );
+                <input
+                type='password'
+                name='password'
+                placeholder='Enter password'
+                onChange={updatePassword}
+                value={password}
+                minLength={5}
+                required={true}
+                ></input>
+
+                <input
+                type='password'
+                name='repeat_password'
+                placeholder='Repeat password'
+                onChange={updateRepeatPassword}
+                value={repeatPassword}
+                minLength={5}
+                required={true}
+                ></input>
+
+            <button type='submit' className='fwb' id='signUpButtons'>Sign Up</button>
+            </form>
+        </div>
+    );
 };
 
 export default SignUpForm;
