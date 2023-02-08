d70aa5a5f7fa22f405885e1725aff924a39e6f68
brandon <brandon.bim13@gmail.com>
2023-02-07 19:21:45 -0500

    added routes for comments

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,3 +3,4 @@ from .user_routes import user_routes
 from .board_routes import board_routes
 from .card_routes import card_routes
 from .list_routes import list_routes
+from .comment_routes import comment_routes
