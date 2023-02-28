34002557c9fdd21740753a6a0ae30f9a9eb434d2
brandon <brandon.bim13@gmail.com>
2023-02-28 13:29:42 -0500

    added to index.css and removed warnings

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink, useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import './SplashPage.css'
 
